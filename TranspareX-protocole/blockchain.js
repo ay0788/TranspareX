e6916040e7a2dbfb7f6c234341182acec1beb952
Aya_Sdour <sdouraya3@gmail.com>
2025-01-25 03:10:08 +0100

    Delete TranspareX-protocole/blockchain.js

--- TranspareX-protocole/blockchain.js
+++
@@ -1,4 +0,0 @@
-import Web3 from 'web3';
-
-const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-export const getContractInstance = (abi, address) => new web3.eth.Contract(abi, address);
